ed67521c169b2c719238f26321e55ab317ab79f3
ggorockee <ggorockee@gmail.com>
2025-06-29 21:31:58 +0900

    Resiponsive Design

---
+++ frontend/src/routes/Home.tsx
@@ -0,0 +1,39 @@
+import { FaStar, FaRegHeart } from "react-icons/fa";
+import {
+  Box,
+  Button,
+  Grid,
+  HStack,
+  Image,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import Room from "../components/Room";
+
+export default function Home() {
+  return (
+      <Grid
+          mt={10}
+          px={{
+            base: 10,
+            lg: 40,
+          }}
+          columnGap={4}
+          rowGap={8}
+          templateColumns={{
+            sm: "1fr",
+            md: "1fr 1fr",
+            lg: "repeat(3, 1fr)",
+            xl: "repeat(4, 1fr)",
+            "2xl": "repeat(5, 1fr)",
+          }}
+      >
+        {[
+          1, 2, 3, 4, 56, 7, 7, 7, 6, 6, 6, 6, 66, 6, 6, 6, 6, 2, 2, 2, 2, 2, 2,
+          2, 2, 2, 2,
+        ].map((index) => (
+            <Room key={index} />
+        ))}
+      </Grid>
+  );
+}
